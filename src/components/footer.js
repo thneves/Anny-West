6ededdd2c1850b26b39fa2b1997f004ecc58e0bf
thales-pinner <thales.neves@pinpeople.com.br>
2023-09-13 19:37:28 -0300

    Create footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,16 @@
 const Footer = () => {
   return (
     <footer>
-      <p>© 2021, Built with Gatsby</p>
+      <div className="triangle">
+        <div className='triangle'></div>
+        <h1 className='text-text_dark text-6xl font-semibold mt-4'>ANNY WEST</h1>
+        <h2 className='text-text_dark text-4xl font-light font-montserrat tracking-widest'>modelo</h2>
+      </div>
+      <div className="font-montserrat font-light font-1xl">
+        <span >@annywest</span>
+        <span>(61)98573-4655</span>
+        <span>annywestsoares@gmail.com</span>
+      </div>
     </footer>
   )
 }
