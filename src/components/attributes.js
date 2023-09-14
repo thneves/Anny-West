e87c159d57660c9a2e9e13f7b91c8c262b1d22fc
thales-pinner <thales.neves@pinpeople.com.br>
2023-09-13 21:54:41 -0300

    Finish body attribtutes section

--- src/components/attributes.js
+++ src/components/attributes.js
@@ -8,27 +8,27 @@ const Attributes = () => {
         <div className="w-2/5">
           <Image src={charsImg} quality={100} className='h-screen'/>
         </div>
-        <div className="flex flex-col items-center justify-center mx-auto w-3/5 bg-primary text-text_dark">
-          <div className='flex flex-row items-center justify-start'>
+        <div className="flex flex-col pt-28 pl-16 mx-auto w-3/5 bg-primary text-text_dark">
+          <div className='flex flex-row justfity-start mb-2'>
             <AiFillCaretDown color='#b16c51' size={80} />
-            <h2 className='text-text_dark text-5xl font-semibold mt-4'>CARACTERÍSTICAS FÍSICAS</h2>
+            <h2 className='text-text_dark text-5xl font-semibold mt-1'>CARACTERÍSTICAS<br></br> FÍSICAS</h2>
           </div>
-          <div>
-            <div>
-                <span>Altura</span>
-                <span>1,70cm</span>
+          <div className='grid grid-cols-2 gap-4 ml-20 mt-7'>
+            <div className='flex flex-col'>
+                <span className='font-bold text-4xl tracking-widest'>Altura</span>
+                <span className='font-light font-montserrat text-3xl tracking-widest'>1,70cm</span>
             </div>
-            <div>
-                <span>Vestuário</span>
-                <span>P ou 38</span>
+            <div className='flex flex-col'>
+                <span className='font-bold text-4xl tracking-widest'>Vestuário</span>
+                <span className='font-light font-montserrat text-3xl tracking-widest'>P ou 38</span>
             </div>
-            <div>
-                <span>Peso</span>
-                <span>56kg</span>
+            <div className='flex flex-col'>
+                <span className='font-bold text-4xl tracking-widest'>Peso</span>
+                <span className='font-light font-montserrat text-3xl tracking-widest'>56kg</span>
             </div>
-            <div>
-                <span>Sapatos</span>
-                <span>37</span>
+            <div className='flex flex-col'>
+                <span className='font-bold text-4xl tracking-widest'>Sapatos</span>
+                <span className='font-light font-montserrat text-3xl tracking-widest'>37</span>
             </div>
           </div>
         </div>
