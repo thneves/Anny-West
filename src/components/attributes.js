15237b0365c26f66bc701c3625828bbd6bc66240
thales-pinner <thales.neves@pinpeople.com.br>
2023-09-16 16:30:20 -0300

    fix

--- src/components/attributes.js
+++ src/components/attributes.js
@@ -5,17 +5,17 @@ import { AiFillCaretDown } from 'react-icons/ai';
 const Attributes = () => {
     return (
         <div className="flex flex-row h-screen">
-        <div className="w-2/5">
+        <div className="w-1/2 md:w-2/5">
           <Image src={charsImg} quality={100} className='h-screen'/>
         </div>
-        <div className="relative flex flex-col pt-28 pl-16 mx-auto w-3/5 bg-primary text-text_dark">
+        <div className="relative flex flex-col pt-4 md:pt-28 md:pl-16 mx-auto w-1/2 md:w-3/5 bg-primary text-text_dark">
 
         <div className='absolute left-0 bottom-0 w-4 h-2/5 bg-icon'></div>
-          <div className='flex flex-row justfity-start mb-2'>
-            <AiFillCaretDown color='#b16c51' size={80} />
-            <h2 className='text-text_dark text-5xl mt-1 font-lovelo'>CARACTERÍSTICAS<br></br> FÍSICAS</h2>
+          <div className='flex md:flex-row flex-col items-center justfity-start mb-2'>
+            <AiFillCaretDown color='#b16c51' size={30} />
+            <h2 className='text-text_dark text-2xl sm:text-3xl md:text-5xl mt-1 font-lovelo'>CARACTERÍSTICAS<br></br> FÍSICAS</h2>
           </div>
-          <div className='grid grid-cols-2 gap-y-8  ml-20 mt-7 w-3/5'>
+          <div className='md:grid grid-cols-2 gap-y-8  ml-20 mt-7 w-3/5'>
             <div className='flex flex-col'>
                 <span className='font-bold text-3xl tracking-widest mr-[-100px]'>Altura</span>
                 <span className='font-light font-montserrat text-3xl tracking-widest'>1,70cm</span>
