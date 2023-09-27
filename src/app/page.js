ca0d85570e460ad327c51ce809e118c683548d84
thales-pinner <thales.neves@pinpeople.com.br>
2023-09-26 22:01:27 -0300

    ta ok por hoje

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
       <About  />
       <Attributes />
       <NiceToMeetYou />
-      {/*<Facts />
-       <Footer />*/}
+      <Facts />
+       <Footer />
     </main>
   )
 }
--- src/components/nicet_to_meet_you.js
+++ src/components/nicet_to_meet_you.js
@@ -4,9 +4,9 @@ import prazerImg from 'public/static/images/prazer.jpeg'
 const NiceToMeetYou = () => {
   return(
     <div className='bg-half-colors h-screen relative flex'>
-        <h5 className='absolute xs: mt-2 sm:left-28 sm:top-40 font-lovelo text-text_dark sm:text-5xl xs:text-4xl'>PRAZER,</h5>
+        <h5 className='absolute mt-2 ml-2 sm:left-28 sm:top-40 font-lovelo text-text_dark sm:ml-0 sm:mt-0 sm:text-5xl xs:text-4xl'>PRAZER,</h5>
         <div className='xs:hidden absolute bottom-32 w-1/12 h-2/5 bg-icon'></div>
-      <Image src={prazerImg} quality={100} className='h-4/5 sm:w-2/6 mx-auto self-center'/>
+      <Image src={prazerImg} quality={100} className='h-4/5 xs:w-5/6 sm:w-2/6 mx-auto self-center'/>
     </div>
   )
 }
